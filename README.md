16022564bbd1d9ded41e4f1c8d71abcd9568bae4
Darko <darkoristov14@yahoo.com>
2020-03-27 19:47:37 +0100

    added timer and validation functions, missin generating validation numbers

--- README.md
+++ README.md
@@ -1,29 +1,31 @@
 # Sudoku
 Sudoku in terminal
 
-- global variable is_game_on = True                                                                     # DONE
+- global variable is_game_on = True                                                     # DONE
 
-- make the board as a dictionary                                                                         # DONE
+- make the board as a dictionary                                                        # DONE
 
-- while global variable is True:                                                                        # DONE
-  generate random numbers from 1-9 and put them in random positions(~25)                                # DONE
-  and display as a initial board                                                                        # DONE
-  - display the board function                                                                          # DONE
+- while global variable is True:                                                        # DONE
+  generate random numbers from 1-9 and put them in random positions(~25)                # DONE
+  and display as a initial board                                                        # DONE
+  - ( generated numbers follow validation rules )                                       ?
+  - display the board function                                                          # DONE
   
-- input number on the board and specific position:                                                      # DONE
-  check if number is from 1-9 and position is in the dictionary as a key                                # DONE
+- input number on the board and specific position:                                      # DONE
+  check if number is from 1-9 and position is in the dictionary as a key                # DONE
 
-- function for not overriding a position                                                                # DONE
+- function for not overriding a position                                                # DONE
 
-- check if position is valid:                                                                           ?
-  check rows, columns, box                                                                              ?  
-  - if it is place number on board and displayBoard()                                                   ?
-  - else print "You cant place there"                                                                   ?
+- check if position is valid:                                                           # DONE
+  check rows, columns, box                                                              # DONE  
+  ( check if valid as a condition for the generating function and                       # DONE
+    at the end to see if Win or Lose )
+    ( dont deny the player to place number at any empty position )                      # DONE
 
-- game is over function:                                                                                # DONE
-  global variable is False                                                                              # DONE
-  print "Well done."                                                                                    # DONE   
+- game is over function:                                                                # DONE
+  global variable is False                                                              # DONE
+  print "Well done."                                                                    # DONE   
    
-- Backtracking algorithm for solving the game                                                           ?    
+- Backtracking algorithm for solving the game                                           ?    
 
-  - Maybe a timer function                                                                              ?      
+  - Maybe a timer function                                                              #DONE      
--- sudoku.py
+++ sudoku.py
@@ -15,6 +15,7 @@
 ##                                                                                                                       ## 
 ###########################################################################################################################
 import random
+from timeit import default_timer as timer
 
 board = {
     11: "-", 12: "-", 13: "-", 14: "-", 15: "-", 16: "-", 17: "-", 18: "-", 19: "-",
@@ -48,7 +49,7 @@ def displayBoard():
     print("    ", "1  ", "2  ", "3  ", "4  ", "5  ", "6  ", "7  ", "8  ", "9  " )
     print()
 
-
+start = timer()
 def play_game():
     generate_random_numbers()
     displayBoard()
@@ -75,42 +76,64 @@ def play_game():
             print("The number needs to be from 1 to 9!")
 
         check_if_table_is_full()
+        
+
 
 def check_valid_position():
     check_row()
     check_column()
     check_box()
+            
+        
 
 def check_row():
-    row_1 = [ board.get(11), board.get(12), board.get(13), board.get(14), board.get(15), board.get(16), board.get(17), board.get(18).board.get(19) ]
-    row_2 = [ board.get(21), board.get(22), board.get(23), board.get(24), board.get(25), board.get(26), board.get(27), board.get(28).board.get(29) ]
-    row_3 = [ board.get(31), board.get(32), board.get(33), board.get(34), board.get(35), board.get(36), board.get(37), board.get(38).board.get(39) ]
-    row_4 = [ board.get(41), board.get(42), board.get(43), board.get(44), board.get(45), board.get(46), board.get(47), board.get(48).board.get(49) ]
-    row_5 = [ board.get(51), board.get(52), board.get(53), board.get(54), board.get(55), board.get(56), board.get(57), board.get(58).board.get(59) ]
-    row_6 = [ board.get(61), board.get(62), board.get(63), board.get(64), board.get(65), board.get(66), board.get(67), board.get(68).board.get(69) ]
-    row_7 = [ board.get(71), board.get(72), board.get(73), board.get(74), board.get(75), board.get(76), board.get(77), board.get(78).board.get(79) ]
-    row_8 = [ board.get(81), board.get(82), board.get(83), board.get(84), board.get(85), board.get(86), board.get(87), board.get(88).board.get(89) ]
-    row_9 = [ board.get(91), board.get(92), board.get(93), board.get(94), board.get(95), board.get(96), board.get(97), board.get(98).board.get(99) ]
+    row_1 = [ board[11], board[12], board[13], board[14], board[15], board[16], board[17], board[18], board[19] ]
+    row_2 = [ board[21], board[22], board[23], board[24], board[25], board[26], board[27], board[28], board[29] ]
+    row_3 = [ board[31], board[32], board[33], board[34], board[35], board[36], board[37], board[38], board[39] ]
+    row_4 = [ board[41], board[42], board[43], board[44], board[45], board[46], board[47], board[48], board[49] ]
+    row_5 = [ board[51], board[52], board[53], board[54], board[55], board[56], board[57], board[58], board[59] ]
+    row_6 = [ board[61], board[62], board[63], board[64], board[65], board[66], board[67], board[68], board[69] ]
+    row_9 = [ board[91], board[92], board[93], board[94], board[95], board[96], board[97], board[98], board[99] ]
+    row_7 = [ board[71], board[72], board[73], board[74], board[75], board[76], board[77], board[78], board[79] ]
+    row_8 = [ board[81], board[82], board[83], board[84], board[85], board[86], board[87], board[88], board[89] ]
 
     rows = [row_1, row_2, row_3, row_4, row_5, row_6, row_7, row_8, row_9]
 
     for row in rows:
-        pass
+        for i in row:
+            counter = 0
+            for j in row:
+                if j == i:
+                    counter =+ 1
+            if counter == 0:
+                return True
+            else: 
+                return False     
+            
 
 def check_column():
-    column_1 = [ board.get(11), board.get(21), board.get(31), board.get(41), board.get(51), board.get(61), board.get(71), board.get(81).board.get(91) ]
-    column_2 = [ board.get(12), board.get(22), board.get(32), board.get(42), board.get(52), board.get(62), board.get(72), board.get(82).board.get(92) ]
-    column_3 = [ board.get(13), board.get(23), board.get(33), board.get(43), board.get(53), board.get(63), board.get(73), board.get(83).board.get(93) ]
-    column_4 = [ board.get(14), board.get(24), board.get(34), board.get(44), board.get(54), board.get(64), board.get(74), board.get(84).board.get(94) ]
-    column_5 = [ board.get(15), board.get(25), board.get(35), board.get(45), board.get(55), board.get(65), board.get(75), board.get(85).board.get(95) ]
-    column_6 = [ board.get(16), board.get(26), board.get(36), board.get(46), board.get(56), board.get(66), board.get(76), board.get(86).board.get(96) ]
-    column_7 = [ board.get(17), board.get(27), board.get(37), board.get(47), board.get(57), board.get(67), board.get(77), board.get(87).board.get(97) ]
-    column_8 = [ board.get(18), board.get(28), board.get(38), board.get(48), board.get(58), board.get(68), board.get(78), board.get(88).board.get(98) ]
-    column_9 = [ board.get(19), board.get(29), board.get(39), board.get(49), board.get(59), board.get(69), board.get(79), board.get(89).board.get(99) ]
+    column_1 = [ board[11], board[21], board[31], board[41], board[51], board[61], board[71], board[81], board[91] ]
+    column_2 = [ board[12], board[22], board[32], board[42], board[52], board[62], board[72], board[82], board[92] ]
+    column_3 = [ board[13], board[23], board[33], board[43], board[53], board[63], board[73], board[83], board[93] ]
+    column_4 = [ board[14], board[24], board[34], board[44], board[54], board[64], board[74], board[84], board[94] ]
+    column_5 = [ board[15], board[25], board[35], board[45], board[55], board[65], board[75], board[85], board[95] ]
+    column_6 = [ board[16], board[26], board[36], board[46], board[56], board[66], board[76], board[86], board[96] ]
+    column_7 = [ board[17], board[27], board[37], board[47], board[57], board[67], board[77], board[87], board[97] ]
+    column_8 = [ board[18], board[28], board[38], board[48], board[58], board[68], board[78], board[88], board[98] ]
+    column_9 = [ board[19], board[29], board[39], board[49], board[59], board[69], board[79], board[89], board[99] ]
 
     columns = [column_1, column_2, column_3, column_4, column_5, column_6, column_7, column_8, column_9]
 
-    pass
+    for col in columns:
+        for i in col:
+            counter = 0
+            for j in col:
+                if j == i:
+                    counter =+ 1
+            if counter == 0:
+                return True
+            else: 
+                return False 
 
 def check_box():
     box_1 = [ board.get(11), board.get(12), board.get(13), board.get(21), board.get(22), board.get(23), board.get(31), board.get(32), board.get(33) ]
@@ -125,36 +148,51 @@ def check_box():
 
     boxes = [box_1, box_2, box_3, box_4, box_5, box_6, box_7, box_8, box_9]
 
-    pass
+    for box in boxes:
+        for i in box:
+            counter = 0
+            for j in box:
+                if j == i:
+                    counter =+ 1
+            if counter == 0:
+                return True
+            else: 
+                return False 
+
+
 
 
 def check_if_table_is_full():
     global gameIsOn
+    global start
     if "-" not in board.values():
         game_is_over()
+        end = timer()
+        time = end - start
+        print("Finished in: ", round(time, 1), " seconds.")
+        print()
+        
 
     
 
 
 def game_is_over():
     global gameIsOn
-    
     print("Game over.")
-    # valid = check_valid_position()
-    # if valid:
-    #     print("Well done!")
-    # else:
-    #     print("Wrong solution!")
+    valid = check_valid_position()
+    if valid:
+        print("Well done!")
+    else:
+        print("Wrong solution!")
     gameIsOn = False
 
+
 def generate_random_numbers():
-    ran = random.sample(list(board), 30)
+    ran = random.sample(list(board), 80)
     nums = [1,2,3,4,5,6,7,8,9]
     for i in ran:
         n = random.choice(nums)
         board[i] = n
-
-
 
 
 
